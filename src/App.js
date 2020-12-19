5a8a6d9be19bdc59ff78459772b0223500f29158
sungalex <cloudallera@gmail.com>
2020-12-19 16:01:04 +0900

    reuseable commponent with jsx+props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+function Food({fav}) {
+return <h2>I like {fav}</h2>
+}
+
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
+      <Food fav="aaa" />
+      <Food fav="bbb" />      
     </div>
   );
 }
