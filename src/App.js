a70274dd172e2b7aee8bfc279e71d708aff96477
sungalex <cloudallera@gmail.com>
2020-12-19 16:38:12 +0900

    dynamic compoment generation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
-function Food({fav}) {
-return <h2>I like {fav}</h2>
+function Food({name, img}) {
+return <div>
+  <h2>I like {name}</h2>
+  <img src={img} />
+</div>
+
 }
+
+const foodILike = [
+  {
+    name: "kimchi",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj406AKZN9ILW8_LqHQL0gyV0cIb6QcYLaHA&usqp=CAU"
+  },
+  {
+    name: "samgiupsal",
+    image: "https://i.pinimg.com/originals/c4/c5/d5/c4c5d5d428634b5ddf1aabd9f98faf60.jpg"
+  },
+  {
+    name: "bibimbab",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA8nrLeU_kj8dStJg54tkAo-VdXZVaAZFp0A&usqp=CAU"
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!</h1>
-      <Food fav="aaa" />
-      <Food fav="bbb" />      
+      {foodILike.map(dish => <Food name={dish.name} img={dish.image} />)}
     </div>
   );
 }
